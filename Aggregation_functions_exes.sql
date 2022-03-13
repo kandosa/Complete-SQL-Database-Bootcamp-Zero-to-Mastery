3a4f06cbfaf50f7f36d9f1848608e78eed031abc
Hai Huang <illiteratehuang@gmail.com>
2022-03-12 22:59:06 -0500

    Update Aggregation_functions_exes.sql

--- Aggregation_functions_exes.sql
+++ Aggregation_functions_exes.sql
@@ -13,8 +13,8 @@ SELECT avg(salary) FROM salaries
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
--- Result: 1965-02-01
-SELECT max(birth_date) FROM employees
+-- Result: 1952-02-01
+SELECT min(birth_date) FROM employees
 
 -- BEFORE YOU START
 /*
@@ -26,7 +26,9 @@ SELECT max(birth_date) FROM employees
 -- Question 1: How many towns are there in France?
 -- Table: Towns
 -- Result: 36684
-SELECT count(id) FROM towns
+SELECT COUNT(id)
+FROM "public"."towns"
+
 
 -- BEFORE YOU START
 /*
@@ -38,16 +40,21 @@ SELECT count(id) FROM towns
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
 -- Result: 238
-SELECT count(isofficial) FROM countrylanguage where isofficial = TRUE
+select count(countrycode)
+from "public"."countrylanguage"
+where isofficial = true
 
 
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
 -- Result: 66.48603611164265
-SELECT avg(lifeexpectancy) FROM country
-
+select avg("public"."country"."lifeexpectancy")
+from "public"."country"
 
 -- Question 3: What is the average population for cities in the Netherlands?
 -- Table: city
 -- Result: 185001.750000000000
-SELECT avg(city.population) FROM city where countrycode = 'NLD'
+SELECT avg(population)
+FROM "public"."city"
+where countrycode = 'NLD'
+
